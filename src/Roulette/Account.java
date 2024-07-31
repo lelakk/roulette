0e3d758a1adbb3a234d765e53d50acf9dcc49652
lelakk <pawelelekowski@gmail.com>
2024-07-31 22:28:36 +0200

    Added base code

---
+++ src/Roulette/Account.java
@@ -0,0 +1,22 @@
+package Roulette;
+
+public class Account {
+    private int accountBalance;
+
+    public class myAccount{
+    }
+    public int accountBalance(){
+        return accountBalance;
+    }
+    public void setAccountBalance(int accountBalance) {
+        this.accountBalance = accountBalance;
+    }
+
+    public int getAccountBalance() {
+        return accountBalance;
+    }
+
+
+
+}
+
